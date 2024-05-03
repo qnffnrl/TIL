a1ef6607a2bc9aa1b6918a70063ad192e0ccd4a2
risker <roakfwk30@naver.com>
2024-05-04 02:04:45 +0900

    2024-05-04: stack solved 2

---
+++ M202404/CT20240504.java
@@ -0,0 +1,35 @@
+package M202404;
+
+import java.io.*;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class CT20240504 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int cnt = Integer.parseInt(br.readLine());
+
+        for (int i = 0, k = 1; i < cnt; i++, k++) {
+            Stack<String> st = new Stack<>();
+            String sentence = br.readLine();
+            StringTokenizer token = new StringTokenizer(sentence, " ");
+
+            while (token.hasMoreTokens()){
+                st.push(token.nextToken());
+            }
+            String work = "";
+            while (!st.isEmpty()){
+                work = work.concat(st.pop() + " ");
+            }
+            work = "Case #" + k + ": " + work.stripTrailing();
+            bw.write(work + "\n");
+        }
+        bw.flush();
+        bw.close();
+    }
+
+}
---
+++ M202404/CT20240504_1.java
@@ -0,0 +1,35 @@
+package M202404;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class CT20240504_1 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        Stack<Integer> st = new Stack<>();
+
+        int cnt = sc.nextInt();
+
+            for(int i = 0; i < cnt; i++) {
+
+                int k = sc.nextInt();
+                if (k == 0) {
+                    st.pop();
+                } else {
+                    st.push(k);
+                }
+            }
+
+            if (st.isEmpty()) {
+                System.out.println(0);
+                return;
+            }
+            int sum = 0;
+            while (!st.isEmpty()){
+                int num = st.pop();
+                sum += num;
+            }
+            System.out.println(sum);
+    }
+}
